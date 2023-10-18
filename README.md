a69aec96e04f920f12471bdc5ab0f5425874217f
DP5-Frederick-038 <godivafrederick@gmail.com>
2023-10-18 11:20:42 +0700

    Final

---
+++ README.md
@@ -0,0 +1,16 @@
+﻿# RGB-Clock_UTS_KGV_Wiyata
+
+## RGB Clock
+
+Untuk menjalankan program ini terdapat beberapa konfigurasi yang harus dilakukan:
+
+1. Gunakan <em>command</em> <strong>git clone https://github.com/FrederickGodiva/RGB-Clock_UTS_KGV_Wiyata.git</strong> untuk menduplikasi semua file yang ada di repository ini ke local device Anda.
+
+2. Pastikan bahwa Anda sudah memasukkan <strong>GLFW</strong> dan <strong>GLEW</strong> ke dalam folder <em><strong>Dependencies</em></strong>
+
+3. Klik kanan pada Solution dan pilih <em>Properties</em>.<br>
+   a. Pada menu <strong>Configuration Properties</strong>, submenu <em>VC++ Directories<em>, tambahkan <strong>$(SolutionDir)Dependencies\GLFW\include</strong> dan <strong>$(SolutionDir)Dependencies\GLEW\include</strong> pada bagian <em>Include Directories</em><br>
+   b. Tambahkan juga <strong>$(SolutionDir)Dependencies\GLFW\lib</strong> dan <strong>$(SolutionDir)Dependencies\GLEW\lib</strong> pada bagian <em>Library Directories</em><br>
+   c. Pada menu <strong>C/C++</strong>, submenu <em>Preprocessor</em>, tambahkan <strong>GLEW_STATIC</strong> pada <em>Preprocessor Definitions</em><br>
+   d. Pada menu <strong>Linker</strong>, submenu <em>Input</em>, tambahkan <strong>glfw3.lib; opengl32.lib; glew32s.lib</strong> pada <em>Additional Dependencies</em><br>
+   e. Pastikan bahwa <strong>Platform</strong> sudah menggunakan <strong><em>x64</em></strong>
---
+++ main.cpp
@@ -0,0 +1,229 @@
+// main.cpp : This file contains the 'main' function.Program execution begins and ends there.
+//
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <math.h>
+#include "Utility.h"
+
+float velocitySecondPointer = -6.0f / 2;
+float velocityMinutePointer = -0.1f / 2;
+float velocityHourPointer = -0.0083333f / 2;
+
+float currentSecondPointer = 270.0f;
+float currentMinutePointer = 270.0f;
+float currentHourPointer = 270.0f;
+
+float currentTime = 0.0f;
+float lastTime = 0.0f;
+float deltaTime = 0.0f;
+
+float inputHour, inputMinute, inputSecond;
+
+float speed = 1.0f;
+
+unsigned int program;
+
+GLint color1Loc, color2Loc, color3Loc;
+
+float color1[] = { 1.0, 0.0, 0.0, 1.0 };
+float color2[] = { 0.0, 1.0, 0.0, 1.0 };
+float color3[] = { 0.0, 0.0, 1.0, 1.0 };
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_F && action == GLFW_PRESS)
+    {
+        std::cout << "F is pressed" << std::endl;
+        speed = 4.0f;
+    }
+    else if (key == GLFW_KEY_S && action == GLFW_PRESS)
+    {
+        std::cout << "S is pressed" << std::endl;
+        speed = 1.0f;
+    }
+    else if (key == GLFW_KEY_C && action == GLFW_PRESS)
+    {
+        std::cout << "C is pressed" << std::endl;
+        for (int i = 0; i < 4; i++)
+        {
+            color1[i] = (rand() * 1.0f) / RAND_MAX;
+            color2[i] = (rand() * 1.0f) / RAND_MAX;
+            color3[i] = (rand() * 1.0f) / RAND_MAX;
+        }
+
+        glUniform4f(color1Loc, color1[0], color1[1], color1[2], color1[3]);
+        glUniform4f(color2Loc, color2[0], color2[1], color2[2], color2[3]);
+        glUniform4f(color3Loc, color3[0], color3[1], color3[2], color3[3]);
+    }
+    else if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS)
+    {
+        std::cout << "Right arrow is pressed" << std::endl;
+        glfwSetTime(currentTime + 3600);
+    }
+    else if (key == GLFW_KEY_LEFT && action == GLFW_PRESS)
+    {
+        std::cout << "Left arrow is pressed" << std::endl;
+        glfwSetTime(currentTime - 3600);
+    }
+    else if (key == GLFW_KEY_UP && action == GLFW_PRESS)
+    {
+        std::cout << "Up arrow is pressed" << std::endl;
+        glfwSetTime(currentTime + 60);
+    }
+    else if (key == GLFW_KEY_DOWN && action == GLFW_PRESS)
+    {
+        std::cout << "Down arrow is pressed" << std::endl;
+        glfwSetTime(currentTime - 60);
+    }
+    else if(key == GLFW_KEY_T && action == GLFW_PRESS) {
+        float inputHour, inputMinute, inputSecond;
+        std::cout << "C is pressed" << std::endl;
+        std::cout << "Jam : "; 
+        std::cin >> inputHour;
+
+        std::cout << "Menit : "; 
+        std::cin >> inputMinute;
+
+        std::cout << "Detik : ";
+        std::cin >> inputSecond;
+
+        float hour = inputHour * 3600;
+        float minute = inputMinute * 60;
+        float second = inputSecond;
+
+        glfwSetTime(hour + minute + second); 
+        cout << "Time Changed Successfully" << endl;
+    }
+}
+
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "RGB Clock", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    glfwSetKeyCallback(window, keyCallback);
+
+    GLenum err = glewInit();
+
+    float vertices[] = {
+        1.0f, 1.0f,
+        -1.0f, 1.0f,
+        -1.0f, -1.0f,
+        1.0f, -1.0f
+    };
+
+    unsigned int indexArr[] = {
+        0, 1, 2,
+        0, 2, 3
+    };
+
+    unsigned int vertexBuffer = 0;
+    glGenBuffers(1, &vertexBuffer);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 8, vertices, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+
+    unsigned int indexBuffer = 0;
+    glGenBuffers(1, &indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * 6, indexArr, GL_STATIC_DRAW);
+
+    string vertexString = readFile("vertex.vert");
+    string fragmentString = readFile("fragment.frag");
+
+    const char* vertexChar = vertexString.c_str();
+    const char* fragmentChar = fragmentString.c_str();
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(vertexShader, 1, &vertexChar, NULL);
+    glShaderSource(fragmentShader, 1, &fragmentChar, NULL);
+
+    glCompileShader(vertexShader);
+    glCompileShader(fragmentShader);
+
+    program = glCreateProgram();
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+    glLinkProgram(program);
+
+    glUseProgram(program);
+
+    GLint currentSecondPointerLoc = glGetUniformLocation(program, "currentSecondPointer");
+    GLint currentMinutePointerLoc = glGetUniformLocation(program, "currentMinutePointer");
+    GLint currentHourPointerLoc = glGetUniformLocation(program, "currentHourPointer");
+
+    color1Loc = glGetUniformLocation(program, "color1");
+    glUniform4f(color1Loc, color1[0], color1[1], color1[2], color1[3]);
+
+    color2Loc = glGetUniformLocation(program, "color2");
+    glUniform4f(color2Loc, color2[0], color2[1], color2[2], color2[3]);
+
+    color3Loc = glGetUniformLocation(program, "color3");
+    glUniform4f(color3Loc, color3[0], color3[1], color3[2], color3[3]);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        currentTime = glfwGetTime();
+        deltaTime = (currentTime - lastTime) * speed;
+        lastTime = currentTime;
+        
+        currentSecondPointer += (deltaTime * velocitySecondPointer);
+        currentMinutePointer += (deltaTime * velocityMinutePointer);
+        currentHourPointer += (deltaTime * velocityHourPointer);
+
+        glUniform1f(currentSecondPointerLoc, currentSecondPointer * 3.14159265f / 180.0f);
+        glUniform1f(currentMinutePointerLoc, currentMinutePointer * 3.14159265f / 180.0f);
+        glUniform1f(currentHourPointerLoc, currentHourPointer * 3.14159265f / 180.0f);
+
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
